0c74278367dffd0b5277e79731ae8dc8ef6a5a49
Maggie <maggieli99@ucla.edu>
2020-04-15 21:51:39 -0700

    Initial Commit

---
+++ pic10c-hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pic10c-hw1
+//
+//  Created by Maggie Lee on 2020/4/15.
+//  Copyright © 2020 Maggie Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
